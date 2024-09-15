d965841573a0fbab9dea59b07028599d1ff6389c
Fabian Gutierrez <programador5859@gmail.com>
2024-09-14 20:32:50 -0500

    update requirements.txt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 import pandas as pd
 
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import StandardScaler
 
